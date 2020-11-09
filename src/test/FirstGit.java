e1017bab2902ba4131e0aa41dff3ae360498448a
Krzysztof Wiechec <krzysztof.wiechec@ericsson.com>
2020-11-09 09:52:15 +0100

    no code review
    Change-Id: I3ac92fa45a3c4d6497e0ac14a7914bceca264afc

--- src/test/FirstGit.java
+++ src/test/FirstGit.java
@@ -5,7 +5,7 @@ public class FirstGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello from git");
-System.out.println("Hello from gitHub");
+System.out.println("Hello from no code review");
 
 	}
 
