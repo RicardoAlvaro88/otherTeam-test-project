e6b7495bbaba2dd3ac6a5f299c72912edbb2bd3a
Krzysztof Wiechec <krzysztof.wiechec@ericsson.com>
2020-11-13 08:10:38 +0100

    newBranch2

--- src/test/FirstGit.java
+++ src/test/FirstGit.java
@@ -4,10 +4,7 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello from git");
-System.out.println("Push frome code!!!!!");
-System.out.println("B");
-System.out.println("C");
+System.out.println("new_branch2");
 	}
 
 }
