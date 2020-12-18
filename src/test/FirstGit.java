afc11f4d895ff4ebf08d7b00e342395906f6fc7e
RicardoAlvaro88 <74039701+RicardoAlvaro88@users.noreply.github.com>
2020-12-18 11:36:32 +0100

    Update FirstGit.java

--- src/test/FirstGit.java
+++ src/test/FirstGit.java
@@ -4,8 +4,9 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("A");
-System.out.println("B");					
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
 	}
 
 }
