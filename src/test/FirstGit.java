ac233b5349e53b03db06cf090daa7cadfdea0f0f
RicardoAlvaro88 <74039701+RicardoAlvaro88@users.noreply.github.com>
2021-07-08 14:41:32 +0200

    Update FirstGit.java

--- src/test/FirstGit.java
+++ src/test/FirstGit.java
@@ -5,7 +5,7 @@ public class FirstGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("A");
-		System.out.println("AAA");
+		System.out.println("AAAA");
 		System.out.println("commit + tag");
 	}
 
